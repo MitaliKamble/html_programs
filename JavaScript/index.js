9017a3bd5e1523214026a34ae6393d4bdd159af2
Mitali Kamble <mitalikamble2005@gmail.com>
2025-01-18 15:42:02 +0530

    message

---
+++ JavaScript/index.js
@@ -0,0 +1,19 @@
+//console.log("Welcome to JavaScript code");
+//console.log("This is a console statement");
+//document.write("This is .js code this can't executed in this file")
+
+//Variables in Javascript
+// let a = 4;
+// let b = 4;
+// let sum = a + b;
+// console.log(sum);
+
+// Control Statement
+let a = 10;
+let b = 71;
+
+if(a>b){
+    console.log("a is greater")
+}else{
+    console.log("b is greater")
+}
